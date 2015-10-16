9394ade2c303d394390235f6b3d4dd9d318bd179
Andy C. K <andzandz@users.noreply.github.com>
2015-10-16 15:21:42 +0100

    Andy K: Added Go+neo4j(neoism library) examples.

---
+++ neoism_go_neo4j_examples/junctionQuery.go
@@ -0,0 +1,73 @@
+//REST URL: http://carsimulator.sb05.stations.graphenedb.com:24789/db/data/
+//VIEW URL: http://carsimulator.sb05.stations.graphenedb.com:24789/browser/
+//USERNAME: car_simulator
+//PASSWORD: ABrUgUpyCjgBJDCWeWQc
+
+package main
+
+import "fmt"
+import "github.com/jmcvetta/neoism"
+import "strconv"
+
+var db *neoism.Database //The * means this is a pointer to a Database object
+
+func initdb() {
+  //neoism.Connect("http://username:password@server:port")
+  var err error
+  db, err = neoism.Connect("http://car_simulator:ABrUgUpyCjgBJDCWeWQc@carsimulator.sb05.stations.graphenedb.com:24789")
+  //If error connecting to database, halt execution and display error message
+  if(err != nil) { panic(err) }
+  if(db == nil) { panic("Database object is null") }
+}
+
+////////////////////////////////
+
+
+func queryJunction(junctNum int) {
+  stmt := `
+   MATCH (jone {id:"` + strconv.Itoa(junctNum) + `"})-[:ROAD]->(junction)
+   RETURN junction
+  `
+
+	// query results
+	res := []struct {
+		Junction neoism.Node
+	}{}
+
+	// construct query
+	cq := neoism.CypherQuery{
+		Statement:  stmt,
+		Result:     &res,
+	}
+	// execute query
+	err := db.Cypher(&cq)
+	if(err != nil){ panic(err) } //I would prefer exceptions but Go lacks these
+
+	fmt.Printf("query result:\n")
+	for i, _ := range res {
+		n := res[i].Junction // Only one row of data returned
+		fmt.Printf("  Node %d: %+v\n", i, n.Data )
+	}
+  
+}
+
+/////////////////////////////////////////
+
+func main() {
+	fmt.Println("Hello, connecting to gsy database...")
+  initdb()
+  fmt.Print("Which junction number do you want to query (1-9): ")
+  junctNum := cliInputNum()
+  fmt.Println("Querying junctions connected to junction", junctNum, "with ROAD relations: ") //using commas with Print/Println like this puts spaces in between
+  queryJunction(junctNum)
+}
+
+func cliInputNum() int {
+  var numStr string //auto-initialised to "", annoyingly
+  fmt.Scanf("%s", &numStr) //& gets the pointer to - address of - numStr so Scanf can change its value
+  // := will declare these variables to be the correct types returned by Atoi - int and error
+  num, err := strconv.Atoi(numStr) //try to convert the string to int
+  //if error converting the string, halt execution(panic) and display error message
+  if(err != nil) {fmt.Println("Cannot convert str to int"); panic(err)} 
+  return num
+}
---
+++ neoism_go_neo4j_examples/tomHanksFilms.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+import "github.com/jmcvetta/neoism"
+
+var db *neoism.Database //The * means this is a pointer to a Database object
+
+func initdb() {
+  var err error
+  
+  //neoism.Connect("http://username:password@server:port")
+  panic("This script works on the default example films and actors database, you will need that running on localhost. Put your password in the Connect statement below then comment out this panic line. The default user is neo4j")
+  
+  db, err = neoism.Connect("http://neo4j:yourpassword@localhost:7474/")
+  if(err != nil) { panic(err) }
+  if(db == nil) { panic("Database object is null") }
+}
+
+////////////////////////////////
+
+
+func queryTomHanks() {
+  //it only works with the variable name "movie" for some reason - that is the type of the node
+  stmt := `
+   MATCH (tom {name:"Tom Hanks"})-[:ACTED_IN]->(movie)
+   RETURN movie
+  `
+
+	// query results
+	res := []struct {
+		Movie neoism.Node
+	}{}
+
+	// construct query
+	cq := neoism.CypherQuery{
+		Statement:  stmt,
+		Result:     &res,
+	}
+	// execute query
+	err := db.Cypher(&cq)
+	if(err != nil){ panic(err) } //I would prefer exceptions but Go lacks these
+
+	fmt.Printf("query result:\n")
+	for i, _ := range res {
+		n := res[i].Movie // Only one row of data returned
+		fmt.Printf("  Node %d: %+v\n", i, n.Data )
+	}
+  
+}
+
+/////////////////////////////////////////
+
+func main() {
+	fmt.Println("Hello")
+  initdb()
+  fmt.Println("Querying films Tom Hanks acted in:")
+  queryTomHanks()
+}

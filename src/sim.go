a2976f10d8bf2c1a7ef3a00da24a0b813c3318e7
Andy C. Knight <andzandz@users.noreply.github.com>
2015-10-27 21:56:01 +0000

    Added Road and test TestCarDriveOntoARoad

--- src/sim.go
+++ src/sim.go
@@ -1,5 +1,7 @@
 package main
 
+import "container/list"
+
 type Car struct {
   position float32
   velocity float32
@@ -7,4 +9,24 @@ type Car struct {
 
 func moveCar(car *Car, duration float32) {
   car.position += car.velocity*duration
+}
+
+////////////////////////////////
+
+type Road struct {
+  cars *list.List
+}
+
+func newRoad() *Road {
+  road := &Road{ list.New() }
+  return road
+}
+
+func driveCarOntoRoad(car *Car, road *Road) {
+  car.position = 0
+  road.cars.PushBack(car)
+}
+
+func positionOfCar(car *Car) float32 {
+  return car.position
 }
--- src/sim_test.go
+++ src/sim_test.go
@@ -10,3 +10,14 @@ func TestCarCanMove100MetresIn10SecondsAt10MetresPerSecond(t *testing.T) {
   
   if mycar.position != 100 { t.Fatal("car position not 100m after 10s at 10m/s") }
 }
+
+func TestCarCanDriveOntoARoad (t *testing.T) {
+  //make a car with position 0 and velocity 10
+  mycar := &Car{0,10}
+  //make a road
+  myroad := newRoad()
+  //put car onto road
+  driveCarOntoRoad(mycar, myroad)
+  
+  if positionOfCar(mycar) != 0 { t.Fatal("car not at start of road") }
+}

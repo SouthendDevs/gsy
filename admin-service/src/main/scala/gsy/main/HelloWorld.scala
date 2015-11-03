433b24d93e167fb2c676188c7342c0f259990288
sgthuggy <tim@myhuggyfamily.com>
2015-11-03 20:58:25 +0000

    Added appRun to gradle for admin-server

--- admin-service/src/main/scala/gsy/main/HelloWorld.scala
+++ admin-service/src/main/scala/gsy/main/HelloWorld.scala
@@ -1,9 +1,28 @@
 package gsy.main
 
 import org.scalatra._
+import org.json4s.{DefaultFormats, Formats}
+import org.scalatra.json._
 
-class HelloWorld extends ScalatraFilter {
-  get("/") {"Hello world!"}
+case class GsyParams(simName: String, numberOfCars: Int)
 
-  get("/config") {"Your config page goes here!"}
+class HelloWorld extends ScalatraFilter with JacksonJsonSupport {
+  protected implicit val jsonFormats: Formats = DefaultFormats
+
+  before() {
+    contentType = formats("json")
+  }
+
+  get("/") {
+    "Hello world!"
+  }
+
+  get("/config") {
+    "Your config page goes here!"
+  }
+
+  get("/config/params") {
+    GsyParams("Test Sim", 1)
+  }
+
 }
